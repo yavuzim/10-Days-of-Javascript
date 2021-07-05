69fba9c09ff54ac7d8aa24a3e7e8fbd431a71c37
kemal <yavuz.imer133@gmail.com>
2021-07-05 10:51:29 +0300

    day 2

--- Day 2 Loops/problem.js
+++ Day 2 Loops/problem.js
@@ -5,7 +5,7 @@ function vowelsAndConsonants(s) {
     for(let i=0; i<_array.length; i++){
         _array[i]=='a' || _array[i]=='e' || _array[i]=='i' || _array[i]=='o' || _array[i]=='u' ? vowel.splice(i,0,_array[i]) : consonant.splice(i,0,_array[i])
     } 
-    let letters = vowel.join("")+""+consonant.join("");
+   let letters = vowel.join("").concat(consonant.join(""))
     for(let i=0; i<letters.length; i++){
         console.log(letters[i])
     }    
