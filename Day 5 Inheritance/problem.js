d3f0f16e7f2b259415af732e9c85149ffb3a46e0
90539 <yavuz.imer133@gmail.com>
2021-09-13 12:27:56 +0300

    Day 5 Inheritance

--- Day 5 Inheritance/problem.js
+++ Day 5 Inheritance/problem.js
@@ -8,23 +8,24 @@ class Rectangle {
 /*
  *  Write code that adds an 'area' method to the Rectangle class' prototype
  */
-Rectangle.prototype.area=function(){
-  if(this.h==null) return this.w*this.w;
-  else return this.w*this.h;
+Rectangle.prototype.area = function () {
+    let area
+    this.h == null ? area = this.w * this.w : area = this.w * this.h
+    return area
 }
 /*
  * Create a Square class that inherits from Rectangle and implement its class constructor
  */
-class Square extends Rectangle{
-   constructor(w){
-       super(w);
-   }
+class Square extends Rectangle {
+    constructor(w) {
+        super(w);
+    }
 }
 
-if (JSON.stringify(Object.getOwnPropertyNames(Square.prototype)) === JSON.stringify([ 'constructor' ])) {
+if (JSON.stringify(Object.getOwnPropertyNames(Square.prototype)) === JSON.stringify(['constructor'])) {
     const rec = new Rectangle(3, 4);
     const sqr = new Square(3);
-    
+
     console.log(rec.area());
     console.log(sqr.area());
 } else {
